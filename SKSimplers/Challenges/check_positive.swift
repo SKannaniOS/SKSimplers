3afe6a3f209ab1492c692c1fa37a22d22b55c836
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-16 13:29:58 +0530

    feat: check a number is positive or not without any comparison operator

---
+++ SKSimplers/Challenges/check_positive.swift
@@ -0,0 +1,12 @@
+//
+//  Check_Positive.swift
+//  SKSimplers
+//
+//  Created by Satheesh Kannan on 16/02/25.
+//
+
+import Foundation
+
+func isPositive(_ number: Int) -> Bool {
+    return String(number).first != "-"
+}
